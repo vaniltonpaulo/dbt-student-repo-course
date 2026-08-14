d36991a33005c109869344e05625e2e8ef103ca8
Vanilton paulo <120561451+vaniltonpaulo@users.noreply.github.com>
2026-08-14 18:23:31 +0000

    chapter 5 done

--- airbnb/models/src/src_hosts.sql
+++ airbnb/models/src/src_hosts.sql
@@ -1,6 +1,10 @@
 WITH raw_hosts AS (
     SELECT *
-    FROM AIRBNB.RAW.RAW_HOSTS
+    FROM {{ source('airbnb','hosts') }}
+    -- After this you have to hit dbt compile
+    -- With DBT compile, DBT goes through all of your models and YAML files and tests and so on, and it will
+
+ -- check if all of the references and template tags and everything is correct and it says okay
 )
 
 
--- airbnb/models/src/src_listings.sql
+++ airbnb/models/src/src_listings.sql
@@ -1,8 +1,11 @@
 WITH raw_listings AS (
     SELECT
         *
-    FROM
-        AIRBNB.RAW.RAW_LISTINGS
+    FROM {{ source('airbnb','listings') }}
+    -- After this you have to hit dbt compile
+    -- With DBT compile, DBT goes through all of your models and YAML files and tests and so on, and it will
+
+ -- check if all of the references and template tags and everything is correct and it says okay
 )
 SELECT
     id AS listing_id,
